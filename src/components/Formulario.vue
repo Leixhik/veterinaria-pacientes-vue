58b1208255e0ee5ecd89f5cb9dfe057ae4aefb8d
Leixhik <eduardo.leijapl@uanl.edu.mx>
2025-10-21 11:43:34 -0600

    Se agregaron los fields para el registro de los datos del paciente en el componente de Formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -10,6 +10,100 @@
             Añade Pacientes y 
             <span class="text-indigo-600 font-bold">Adminístalos</span>
         </p>
+
+        <form 
+           class="bg-white shadow-md rounded-lg py-10 px-5 mb-10" 
+        >
+            <!-- Campo para Datos Perro -->
+            <div>
+                <label 
+                    for="mascota"
+                    class="block text-gray-700 uppercase font-bold"
+                >
+                    Nombre Mascota
+                </label>
+
+                <input
+                    id="mascota"
+                    type="text"
+                    placeholder="Nombre de la Mascota"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                />
+            </div>
+
+            <!-- Campo para Datos Dueño -->
+            <div>
+                <label 
+                    for="propietario"
+                    class="block text-gray-700 uppercase font-bold"
+                >
+                    Nombre Propietario
+                </label>
+
+                <input
+                    id="mascota"
+                    type="text"
+                    placeholder="Nombre del Propietario"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                />
+            </div>
+            
+            <!-- Campo para Email de Contacto-->
+            <div>
+                <label 
+                    for="email"
+                    class="block text-gray-700 uppercase font-bold"
+                >
+                    Email
+                </label>
+
+                <input
+                    id="email"
+                    type="email"
+                    placeholder="Email del Propietario"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                />
+            </div>
+            
+            <!-- Campo para Alta de Paciente-->
+            <div>
+                <label 
+                    for="alta"
+                    class="block text-gray-700 uppercase font-bold"
+                >
+                    Fecha de Alta
+                </label>
+
+                <input
+                    id="alta"
+                    type="date"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                />
+            </div>
+
+            <!-- Campo para Síntomas-->
+            <div>
+                <label 
+                    for="sintomas"
+                    class="block text-gray-700 uppercase font-bold"
+                >
+                    Fecha de Alta
+                </label>
+
+                <input
+                    id="sintomas"
+                    placeholder="Describe los síntomas del paciente:"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40"
+                />
+            </div>
+
+            <input
+                type="submit"
+                class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+                value="Registrar Paciente"
+            >
+
+        </form>
     </div>
 </template>
 
